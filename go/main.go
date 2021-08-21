056cdd92c9808437b584ee50f5d1925dd8c550c3
Takafumi ONAKA <takafumi.onaka@gmail.com>
2021-08-21 13:13:35 +0900

    Revert "oooops (limit 20)"
    This reverts commit 363136222b4a3dec04a8e296be680fccd035af9d.

--- go/main.go
+++ go/main.go
@@ -1017,7 +1017,7 @@ func getIsuConditionsFromDB(db *sqlx.DB, jiaIsuUUID string, endTime time.Time, c
 		err = db.Select(&conditions,
 			"SELECT * FROM `isu_condition` WHERE `jia_isu_uuid` = ?"+
 				"	AND `timestamp` < ?"+
-				"	ORDER BY `timestamp` DESC"+
+				"	ORDER BY `timestamp` DESC",
 				"	LIMIT 20",
 			jiaIsuUUID, endTime,
 		)
@@ -1026,7 +1026,7 @@ func getIsuConditionsFromDB(db *sqlx.DB, jiaIsuUUID string, endTime time.Time, c
 			"SELECT * FROM `isu_condition` WHERE `jia_isu_uuid` = ?"+
 				"	AND `timestamp` < ?"+
 				"	AND ? <= `timestamp`"+
-				"	ORDER BY `timestamp` DESC"+
+				"	ORDER BY `timestamp` DESC",
 				"	LIMIT 20",
 			jiaIsuUUID, endTime, startTime,
 		)
